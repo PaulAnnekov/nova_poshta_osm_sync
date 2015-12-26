7118b5746c3661b6f107d686c82afda9fd37abc6
Paul Annekov <paul.annekov@gmail.com>
2015-12-27 00:31:45 +0200

    adding branch id search

--- web/app.dart
+++ web/app.dart
@@ -4,77 +4,109 @@ import 'package:leaflet/leaflet.dart';
 import 'package:logging/logging.dart';
 
 LeafletMap map;
-List<Map> npm;
-List<Map> osmm;
+List<Map> npms;
+List<Map> osmms;
 Map<String, Map> locations;
 final Logger log = new Logger('main');
+final BranchesProcessor branchesProcessor = new BranchesProcessor();
 
-displayOsmms() {
+class BranchesProcessor {
+  Map groupedBranches = {};
+
+  _add(String groupId, Map marker, String group) {
+    if (!groupedBranches.containsKey(groupId))
+      groupedBranches[groupId] = {'osmms': [], 'npms': []};
+    groupedBranches[groupId][group].add(osmms);
+  }
+
+  addOsmm(String groupId, Map osmm) {
+    _add(groupId, osmm, 'osmms');
+  }
+
+  addNpm(String groupId, Map osmm) {
+    _add(groupId, osmm, 'npms');
+  }
+}
+
+determineOsmmsBranchId() {
+  osmms = osmms.map((node) {
+    node['tags']['n'] = getOsmmBranchId(node);
+    return node;
+  }).toList();
+}
+
+int getOsmmBranchId(Map osmm) {
+  int idFromBranch;
+  int idFromName;
+  Map<String, String> tags = osmm['tags'];
+  if (tags['branch'] != null)
+  {
+    var matches = tags['branch'].allMatches(r'[0-9]+');
+    if (matches.length > 1)
+      log.warning("osmm $osmm 'branch' tag contains more then one numbers sequence");
+    if (matches.length)
+      idFromBranch = int.parse(matches.first.toString());
+  }
+  var matches = tags['name'].allMatches(r'[0-9]+');
+  if (matches.length > 1)
+    log.warning("osmm $osmm 'name' tag contains more then one numbers sequence");
+  if (matches.length)
+    idFromName = int.parse(matches.first.toString());
+  if (idFromBranch != null && idFromName != null && idFromName != idFromBranch)
+    log.warning("id from 'name' tag is not equal to id from 'branch' tag for $osmm");
+
+  return idFromBranch != null ? idFromBranch : idFromName;
+}
+
+displayMarkers(List<Map> nodes, String color) {
   var markers = new FeatureGroup();
-  osmm.forEach((place) {
-    Map address = getLocation(place['lat'], place['lon'])['address'];
-    String text = '<b>lat</b>: ${place['lat']}<br>' +
-    '<b>lon</b>: ${place['lon']}<br>';
-    place['tags'].forEach((key, value) {
-      text += '<b>' + key + '</b>: ' + value + '<br>';
+  nodes.forEach((node) {
+    Map address = getLocation(node['lat'], node['lon'])['address'];
+    String text = '<b>lat</b>: ${node['lat']}<br>' +
+    '<b>lon</b>: ${node['lon']}<br>';
+    node['tags'].forEach((key, value) {
+      text += '<b>$key</b>: $value<br>';
     });
     text += '<br>';
     address.forEach((key, value) {
-      text += '<b>' + key + '</b>: ' + value + '<br>';
+      text += '<b>$key</b>: $value<br>';
     });
-    CircleMarker marker = new CircleMarker(new LatLng(place['lat'], place['lon']), color: 'blue', fillOpacity: '0')
+    CircleMarker marker = new CircleMarker(new LatLng(node['lat'], node['lon']), color: color, fillOpacity: '0')
       ..bindPopup(text);
     markers.addLayer(marker);
   });
   map.addLayer(markers);
 }
 
-displayNpms() {
-  var markers = new FeatureGroup();
-  npm.forEach((branch) {
-    double lat = double.parse(branch['y']), lon = double.parse(branch['x']);
-    Map address = getLocation(lat, lon)['address'];
-    String text = '<b>lat</b>: ' + branch['y'] + '<br>' +
-    '<b>lon</b>: ' + branch['x'] + '<br>' +
-    '<b># </b>: ' + branch['n'] + '<br>' +
-    '<b>addr</b>: ' + branch['addr'] + '<br>' +
-    '<b>city</b>: ' + branch['city'] + '<br>';
-    text += '<br>';
-    address.forEach((key, value) {
-      text += '<b>' + key + '</b>: ' + value + '<br>';
-    });
-    CircleMarker marker = new CircleMarker(new LatLng(branch['y'], branch['x']), color: 'red', fillOpacity: '0')
-      ..bindPopup(text);
-    markers.addLayer(marker);
-  });
-  map.addLayer(markers);
+String getPlaceId(address) {
+  List namePriority = ['village', 'town', 'city'];
+  String placeTag = namePriority.firstWhere((name) {
+    if (address[name] != null)
+      return true;
+  }, orElse: () => null);
+  if (placeTag == null) {
+    return null;
+  }
+  List nameParts = [address[placeTag]];
+  if (address['county'] != null && placeTag != 'city')
+    nameParts.add(address['county']);
+  if (address['state'] != null)
+    nameParts.add(address['state']);
+  return nameParts.join(' ');
 }
 
 groupByPlace() {
-  List namePriority = ['village', 'town', 'city'];
-  Map groups = {};
-  osmm.forEach((node) {
+  osmms.forEach((node) {
     Map address = getLocation(node['lat'], node['lon'])['address'];
-    String placeTag = namePriority.firstWhere((name) {
-      if (address[name] != null)
-        return true;
-    }, orElse: () => null);
-    if (placeTag == null) {
+    String groupName = getPlaceId(address);
+    if (groupName == null) {
       log.info('Can not find place tag for: $node ($address)');
       return;
     }
-    List nameParts = [address[placeTag]];
-    if (address['county'] != null && placeTag != 'city')
-      nameParts.add(address['county']);
-    if (address['state'] != null)
-      nameParts.add(address['state']);
-    String groupName = nameParts.join(' ');
-    if (!groups.containsKey(groupName))
-      groups[groupName] = [];
-    groups[groupName].add(node);
+    branchesProcessor.addOsmm(groupName, node);
   });
 
-  var markers = new FeatureGroup();
+  /*var markers = new FeatureGroup();
   groups.forEach((id, List<Map> nodes) {
     double minLat = 100.0, maxLat = -1.0, minLon = 100.0, maxLon = -1.0;
     nodes.forEach((Map node) {
@@ -91,7 +123,7 @@ groupByPlace() {
       new LatLng(maxLat, maxLon)])..bindPopup(id);
     markers.addLayer(marker);
   });
-  map.addLayer(markers);
+  map.addLayer(markers);*/
 }
 
 getLocation(double lat, double lon) {
@@ -110,14 +142,24 @@ main() async {
     ..setView(new LatLng(48.45, 31.5), 7);
 
   var response = await HttpRequest.getString('//localhost:8081/npm.json');
-  npm = JSON.decode(response);
+  npms = JSON.decode(response);
   response = await HttpRequest.getString('//localhost:8081/osmm.json');
-  osmm = JSON.decode(response)['elements'];
+  osmms = JSON.decode(response)['elements'];
   response = await HttpRequest.getString('//localhost:8081/locations_cache.json');
   locations = JSON.decode(response);
 
-  displayOsmms();
-  displayNpms();
+  npms = npms.map((branch) {
+    branch['tags'] = {
+      'n': branch['n'],
+      'addr': branch['addr'],
+      'city': branch['city'],
+    };
+    return branch;
+  }).toList();
+
+  determineOsmmsBranchId();
+  displayMarkers(osmms, 'blue');
+  displayMarkers(npms, 'red');
   groupByPlace();
 }
 
