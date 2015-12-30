2fe32bddca49d98da56c8ede216e40d0aba5312a
Paul Annekov <paul.annekov@gmail.com>
2015-12-30 22:08:57 +0200

    more clever cities grouping for NPms

--- web/app.dart
+++ web/app.dart
@@ -1,5 +1,7 @@
 import 'dart:html';
 import 'dart:convert';
+import 'dart:collection';
+import 'dart:math';
 import 'dart:js' as js;
 import 'package:nova_poshta_osm_sync/leaflet/leaflet.dart' as L;
 import 'package:logging/logging.dart';
@@ -8,14 +10,72 @@ L.LeafletMap map;
 L.ControlLayers controlLayers;
 List<Map> npms;
 List<Map> osmms;
-Map<String, Map> locations;
+LocationsProcessor locationsProcessor;
 final Logger log = new Logger('main');
 final BranchesProcessor branchesProcessor = new BranchesProcessor();
+
+double degreeToRadian(double value) {
+  return value * PI / 180;
+}
+
+double calculateDistance(List<double> point1, List<double> point2) {
+  var R = 6371000; // metres
+  var fi1 = degreeToRadian(point1[0]);
+  var fi2 = degreeToRadian(point2[0]);
+  var deltaFi = degreeToRadian(point2[0] - point1[0]);
+  var deltaL = degreeToRadian(point2[1] - point1[1]);
+
+  var a = sin(deltaFi / 2) * sin(deltaFi / 2) + cos(fi1) * cos(fi2) *
+    sin(deltaL / 2) * sin(deltaL / 2);
+  var c = 2 * atan2(sqrt(a), sqrt(1 - a));
+
+  return R * c;
+}
+
+class LocationsProcessor {
+  Map<String, Map> _locations;
+  static List NAME_PRIORITY = ['village', 'town', 'city'];
+
+  LocationsProcessor(Map<String, Map> locations) {
+    _locations = locations;
+  }
+
+  Map getClosestLocationByPlace(String place, List<double> latlon,
+      [num maxDistance = 20000]) {
+    place = place.toLowerCase();
+    Map closestLocation = null;
+    double closestDistance = null;
+    for (String i in _locations.keys) {
+      Map location = _locations[i];
+      Map<String, String> address = location['address'];
+      bool isMatch = LocationsProcessor.NAME_PRIORITY.firstWhere((name) {
+        if (address[name] != null && address[name].toLowerCase() == place)
+          return true;
+      }, orElse: () => null) != null;
+      if (!isMatch)
+        continue;
+      double distance = calculateDistance([latlon[0], latlon[1]],
+          [location['lat'], location['lon']]);
+      if (distance > maxDistance)
+        continue;
+      if (closestDistance == null || distance < closestDistance) {
+        closestDistance = distance;
+        closestLocation = location;
+      }
+    }
+    return closestLocation;
+  }
+
+  getLocation(double lat, double lon) {
+    return _locations['$lat $lon'];
+  }
+}
 
 class BranchesProcessor {
   Map<String, Map<String, List>> groupedBranches = {};
 
   _add(String groupId, Map marker, String group) {
+    groupId = groupId.toLowerCase();
     if (!groupedBranches.containsKey(groupId))
       groupedBranches[groupId] = {'osmms': [], 'npms': []};
     groupedBranches[groupId][group].add(marker);
@@ -67,7 +127,8 @@ int getOsmmBranchId(Map osmm) {
 L.LayerGroup displayMarkers(List<Map> nodes, String color, String layerName) {
   L.LayerGroup layerGroup = L.layerGroup();
   nodes.forEach((node) {
-    Map address = getLocation(node['lat'], node['lon'])['address'];
+    Map address = locationsProcessor.getLocation(node['lat'],
+        node['lon'])['address'];
     String text = '<b>lat</b>: ${node['lat']}<br>' +
     '<b>lon</b>: ${node['lon']}<br>';
     node['tags'].forEach((key, value) {
@@ -85,21 +146,24 @@ L.LayerGroup displayMarkers(List<Map> nodes, String color, String layerName) {
   return layerGroup;
 }
 
-String getPlaceId(address) {
-  List namePriority = ['village', 'town', 'city'];
-  String placeTag = namePriority.firstWhere((name) {
+LinkedHashMap<String, String> getGroupIdParts(address) {
+  String placeTag = LocationsProcessor.NAME_PRIORITY.firstWhere((name) {
     if (address[name] != null)
       return true;
   }, orElse: () => null);
-  if (placeTag == null) {
-    return null;
-  }
-  List nameParts = [address[placeTag]];
+  LinkedHashMap nameParts = new LinkedHashMap();
+  if (placeTag != null)
+    nameParts['place'] = address[placeTag];
   if (address['county'] != null && placeTag != 'city')
-    nameParts.add(address['county']);
+    nameParts['county'] = address['county'];
   if (address['state'] != null)
-    nameParts.add(address['state']);
-  return nameParts.join(' ');
+    nameParts['state'] = address['state'];
+  return nameParts;
+}
+
+String getNPMCity(String city) {
+  var cityParts = city.split('(');
+  return cityParts[0].trim();
 }
 
 List<L.Polygon> getCitiesPolygon(List nodes, String groupId, String color) {
@@ -128,39 +192,52 @@ List<L.Polygon> getCitiesPolygon(List nodes, String groupId, String color) {
 
 groupByPlace() {
   osmms.forEach((node) {
-    Map address = getLocation(node['lat'], node['lon'])['address'];
-    String groupName = getPlaceId(address);
-    if (groupName == null) {
+    Map address = locationsProcessor.getLocation(node['lat'],
+        node['lon'])['address'];
+    var groupParts = getGroupIdParts(address);
+    if (groupParts['place'] == null) {
       log.info('Can not find place tag for: $node ($address)');
       return;
     }
-    branchesProcessor.addOsmm(groupName, node);
+    branchesProcessor.addOsmm(groupParts.values.join(' '), node);
   });
 
   npms.forEach((node) {
-    Map address = getLocation(node['lat'], node['lon'])['address'];
-    String groupName = getPlaceId(address);
-    if (groupName == null) {
-      log.info('Can not find place tag for: $node ($address)');
-      return;
+    Map address = locationsProcessor.getLocation(node['lat'],
+        node['lon'])['address'];
+    var groupParts = getGroupIdParts(address);
+    var isSearch = false;
+    var npmCity = getNPMCity(node['city']);
+    if (groupParts['place'] != null &&
+        groupParts['place'].toLowerCase() != npmCity.toLowerCase()) {
+      log.info('NP city and Nomatim city differs for: $node ($address)');
+      isSearch = true;
     }
-    branchesProcessor.addNpm(groupName, node);
+    if (groupParts['place'] == null) {
+      log.info('Can not find place tag for: $node ($address)');
+      isSearch = true;
+    }
+    if (isSearch) {
+      var location = locationsProcessor.getClosestLocationByPlace(npmCity,
+          [node['lat'], node['lon']]);
+      if (location == null)
+        log.info("Can not find node's city in locations: $node");
+      else
+        groupParts = getGroupIdParts(location['address']);
+    }
+    branchesProcessor.addNpm(groupParts.values.join(' '), node);
   });
 
   L.LayerGroup osmmGroup = L.layerGroup();
   L.LayerGroup npmGroup = L.layerGroup();
   branchesProcessor.groupedBranches.forEach((id, Map nodes) {
-    getCitiesPolygon(nodes['osmms'], id, 'red').forEach((marker)
+    getCitiesPolygon(nodes['osmms'], id, 'blue').forEach((marker)
       => marker.addTo(osmmGroup));
-    getCitiesPolygon(nodes['npms'], id, 'blue').forEach((marker)
+    getCitiesPolygon(nodes['npms'], id, 'red').forEach((marker)
       => marker.addTo(npmGroup));
   });
   controlLayers.addOverlay(osmmGroup, 'OSM cities');
   controlLayers.addOverlay(npmGroup, 'NP cities');
-}
-
-getLocation(double lat, double lon) {
-  return locations['$lat $lon'];
 }
 
 onReady(_) async {
@@ -174,7 +251,7 @@ onReady(_) async {
   response = await HttpRequest.getString('//localhost:8081/osmm.json');
   osmms = JSON.decode(response)['elements'];
   response = await HttpRequest.getString('//localhost:8081/locations_cache.json');
-  locations = JSON.decode(response);
+  locationsProcessor = new LocationsProcessor(JSON.decode(response));
 
   npms = npms.map((branch) {
     branch['tags'] = {
